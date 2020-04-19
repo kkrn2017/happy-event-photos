c136a7a6b30e0d98d4cc9d1f240839e04a6d2963
k0n019d <k0n019d@homeoffice.wal-mart.com>
2020-04-19 17:43:06 -0500

    Initializing Spring boot and Main class

---
+++ src/main/java/com/happyeventphotos/hep/HepApplication.java
@@ -0,0 +1,13 @@
+package com.happyeventphotos.hep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HepApplication.class, args);
+	}
+
+}
